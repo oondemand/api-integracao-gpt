1225376066076d27da87b232c95f1d2ef74f0a0e
Maike <maikonalexandre574@gmail.com>
2025-01-10 18:44:35 -0300

    feat: add system vars and omie vars

--- src/controllers/integracao/question.js
+++ src/controllers/integracao/question.js
@@ -17,10 +17,14 @@ const MENSAGEM_DE_CONTEXTO_PADRAO = {
 
 const MENSAGEM_CONTEXTO_GERACAO_EJS = {
   role: messageType.SYSTEM,
-  content: `Você deve criar o template ejs sem criar a estrutura padrão do html, 
-  caso você crie variáveis lembre se de criar o mock dessas variáveis. 
+  content: `Você deve criar o template ejs sem criar a estrutura padrão do html,  
   Use a estrutura do template ejs fornecido como exemplo, mas não faça igual.
   Você também deve adicionar os estilos css, deixando o design mais fiel possível a imagem.`,
+};
+
+const MENSAGEM_CONTEXTO_VARIAVEIS = {
+  role: messageType.SYSTEM,
+  content: `Voce recebera dois objetos de variais, use-as conforme necessário,`,
 };
 
 const bodySchema = z.object({
@@ -28,16 +32,25 @@ const bodySchema = z.object({
     .string({ message: "A pergunta é um campo obrigatório!" })
     .min(5, { message: "A pergunta precisa ter no mínimo 5 caracteres." }),
   templateEjs: z.string({ message: "Template ejs é um campo obrigatório!" }),
+  omieVar: z.string({ message: "Variáveis omie é um campo obrigatório!" }),
+  systemVar: z.string({
+    message: "Variáveis de sistema é um campo obrigatório!",
+  }),
 });
 
 export const question = async (req, res, next) => {
   try {
-    const { question, templateEjs } = bodySchema.parse(req.body);
+    const { question, templateEjs, omieVar, systemVar } = bodySchema.parse(
+      req.body
+    );
 
     if (!req.file) {
       const messages = [
         MENSAGEM_DE_CONTEXTO_PADRAO,
+        MENSAGEM_CONTEXTO_VARIAVEIS,
         { role: messageType.USER, content: templateEjs },
+        { role: messageType.USER, content: omieVar },
+        { role: messageType.USER, content: systemVar },
         { role: messageType.USER, content: question },
       ];
 
@@ -48,6 +61,9 @@ export const question = async (req, res, next) => {
     const messages = [
       MENSAGEM_DE_CONTEXTO_PADRAO,
       MENSAGEM_CONTEXTO_GERACAO_EJS,
+      MENSAGEM_CONTEXTO_VARIAVEIS,
+      { role: messageType.USER, content: omieVar },
+      { role: messageType.USER, content: systemVar },
       { role: messageType.USER, content: templateEjs },
       {
         role: messageType.USER,
