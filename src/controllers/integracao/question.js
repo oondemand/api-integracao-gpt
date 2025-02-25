4a0584524343c35bbb756a0992fca5c535a4f2a2
Maike <maikonalexandre574@gmail.com>
2025-02-25 13:28:13 -0300

    feat: add ejs dependency and update question controller for template rendering

--- src/controllers/integracao/question.js
+++ src/controllers/integracao/question.js
@@ -1,11 +1,12 @@
 import { z } from "zod";
 import { OpenIaService } from "../../service/openia.js";
+import ejs from "ejs";
 
 const promptSchema = z.object({
-  codigo: z.string(),
+  codigo: z.string().optional(),
   conteudo: z.string(),
   descricao: z.string().optional(),
-  nome: z.string(),
+  nome: z.string().optional(),
   ordem: z.number(),
   tipo: z.string(),
 });
@@ -34,39 +35,29 @@ export const question = async (req, res, next) => {
     const { question, templateEjs, omieVar, systemVar, prompts } =
       bodySchema.parse(req.body);
 
-    const concatenatedMessages = prompts.map((prompt) => {
-      if (prompt.codigo === "CONTEXTO_VARIAVEIS_OMIE" && omieVar) {
-        prompt.conteudo = prompt.conteudo + omieVar;
-      }
+    const concatenatedMessages = [];
 
-      if (prompt.codigo === "CONTEXTO_VARIAVEIS_TEMPLATE" && templateEjs) {
-        prompt.conteudo = prompt.conteudo + templateEjs;
-      }
-
-      if (prompt.codigo === "CONTEXTO_VARIAVEIS_SISTEMA" && systemVar) {
-        prompt.conteudo = prompt.conteudo + systemVar;
-      }
-
-      if (!req.file && prompt.codigo === "CONTEXTO_DE_CHAT") {
-        prompt.conteudo = prompt.conteudo + question;
-      }
-
+    for (const prompt of prompts) {
       if (req.file && prompt.codigo === "CONTEXTO_DE_IMAGEM") {
-        prompt.conteudo = [
-          {
-            type: "image_url",
-            image_url: {
-              url: `data:image/png;base64,${req.file.buffer.toString(
-                "base64"
-              )}`,
-            },
-          },
-          { type: "text", text: question ? question : prompt.conteudo },
-        ];
+        continue;
       }
 
-      return prompt;
-    });
+      const template = await ejs.render(
+        prompt?.conteudo,
+        {
+          ...JSON.parse(omieVar),
+          ...JSON.parse(systemVar),
+          template: templateEjs,
+          conteudo: templateEjs,
+          omie: JSON.parse(omieVar),
+          sistema: JSON.parse(systemVar),
+        },
+        { async: true }
+      );
+
+      prompt.conteudo = template;
+      concatenatedMessages.push(prompt);
+    }
 
     const orderedAndRefactoredMessages = concatenatedMessages
       .sort((a, b) => a.ordem - b.ordem)
@@ -74,26 +65,12 @@ export const question = async (req, res, next) => {
         return { role: e.tipo, content: e.conteudo };
       });
 
-    if (!req.file) {
-      if (!concatenatedMessages.some((e) => e.codigo === "CONTEXTO_DE_CHAT")) {
-        orderedAndRefactoredMessages.push({
-          role: "user",
-          content: question || "",
-        });
-      }
+    if (req.file) {
+      const imgContext = prompts.find(
+        (e) => e?.codigo === "CONTEXTO_DE_IMAGEM"
+      )?.conteudo;
 
-      console.log("MENSAGENS ENVIADAS ->", orderedAndRefactoredMessages);
-
-      const response = await OpenIaService.openSession({
-        messages: orderedAndRefactoredMessages,
-      });
-      console.log("RESPONSE ->", response);
-
-      return res.status(200).json({ message: "ok", data: response });
-    }
-
-    if (!concatenatedMessages.some((e) => e.codigo === "CONTEXTO_DE_IMAGEM")) {
-      orderedAndRefactoredMessages.push({
+      const imageMessage = {
         role: "user",
         content: [
           {
@@ -104,9 +81,11 @@ export const question = async (req, res, next) => {
               )}`,
             },
           },
-          { type: "text", text: question || "" },
+          { type: "text", text: imgContext || question || "" },
         ],
-      });
+      };
+
+      orderedAndRefactoredMessages.push(imageMessage);
     }
 
     console.log("MENSAGENS ENVIADAS ->", orderedAndRefactoredMessages);
