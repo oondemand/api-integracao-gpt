2b169c3d56125ff51a00054bc4d332f546b4f981
Maike <maikonalexandre574@gmail.com>
2025-05-05 15:35:22 -0300

    feat: remove createStream function and its route from IntegracaoController

--- src/controllers/integracao/index.js
+++ src/controllers/integracao/index.js
@@ -1,7 +1,5 @@
-import { createStream } from "./create-stream.js";
 import { question } from "./question.js";
 
 export const IntegracaoController = {
-  createStream,
   question,
 };
--- src/routes/integracaoOpenIa.js
+++ src/routes/integracaoOpenIa.js
@@ -22,7 +22,6 @@ const upload = multer({
   limits: { fileSize: 5 * 1024 * 1024 }, // 5 mb
 });
 
-integracaoOpenIaRoute.post("/create-stream", IntegracaoController.createStream);
 integracaoOpenIaRoute.post(
   "/question",
   upload.array("file"),
